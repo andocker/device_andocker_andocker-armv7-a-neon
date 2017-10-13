ea6ff39067a9ee15ad01ea093d3b0be950c0ec6b
You-Sheng Yang <vicamo@gmail.com>
2017-10-13 23:47:27 +0800

    add product definition

---
+++ andocker_arm.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_arm.mk)
+
+include device/andocker/common/andocker.mk
+
+PRODUCT_NAME := andocker_arm
+PRODUCT_DEVICE := andocker-armv7-a-neon
+PRODUCT_MODEL := andocker-arm
